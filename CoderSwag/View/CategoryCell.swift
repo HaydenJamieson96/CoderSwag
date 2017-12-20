5fb679af529457dae7ded3edeb3567be7a9011e5
HaydenJamieson96 <32482976+HaydenJamieson96@users.noreply.github.com>
2017-12-20 14:49:14 +0000

    Category VC ui setup

---
+++ CoderSwag/View/CategoryCell.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryCell.swift
+//  CoderSwag
+//
+//  Created by TheAppExperts on 12/20/17.
+//  Copyright © 2017 TheAppExperts. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryTitle: UILabel!
+    @IBOutlet weak var categoryImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+
+}
